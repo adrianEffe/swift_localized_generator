9569932c8e3079a0ad4e1d56d9c915667a772c11
adrianEffe <adrian.iosdev@gmail.com>
2023-05-01 17:13:08 +0300

    Implement BufReader for performance

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod parser;
 
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Read;
+
 
 fn main() {
     let file_path = env::args().nth(1).expect("Please provide a file path");
+    let file = File::open(file_path).expect("Failed to open file at given path");
+    let mut buf_reader = BufReader::new(file);
+    let mut content = String::new();
+    buf_reader.read_to_string(&mut content).expect("Failed to read to string");
 
-    let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let keys = parser::parse(&content);
     dbg!(keys);
 }
