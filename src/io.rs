98aa4b06ad49f02803f676a08eebb51925cd438a
adrianEffe <adrian.iosdev@gmail.com>
2023-05-03 21:49:04 +0300

    Format io for consistency

--- src/io.rs
+++ src/io.rs
@@ -1,23 +1,25 @@
 use std::fs;
 use std::fs::File;
-use std::io::{ BufReader, Read};
+use std::io::{BufReader, Read};
 
 pub fn read(file_path: &str) -> String {
     let file = File::open(file_path).expect("Failed to open file at given path");
     let mut buf_reader = BufReader::new(file);
     let mut content = String::new();
-    buf_reader.read_to_string(&mut content).expect("Failed to read to string");
+    buf_reader
+        .read_to_string(&mut content)
+        .expect("Failed to read to string");
     content
 }
 
-pub fn write(data: &str, file_path: &str){
+pub fn write(data: &str, file_path: &str) {
     fs::write(file_path, data).expect("Failed to write to file");
 }
 
 #[cfg(test)]
 mod test {
-    use tempfile::tempdir;
     use super::*;
+    use tempfile::tempdir;
 
     #[test]
     fn test_io_write_read() {
