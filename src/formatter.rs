6d0a55b3a9e93853a62abbf1c9dfa544f4143d2c
adrianEffe <adrian.iosdev@gmail.com>
2023-05-06 19:04:19 +0300

    Rename localisable case for clarity

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,13 +1,13 @@
 pub fn format(keys: Vec<&str>) -> String {
-    let mut swift_file = "".to_string();
+    let mut localizable_case = "".to_string();
     let indentation = "    ";
     for key in keys {
-        swift_file = format!("{}{}{}{}{}", swift_file, indentation, "case ", key, "\n");
+        localizable_case = format!("{}{}{}{}{}", localizable_case, indentation, "case ", key, "\n");
     }
 
     format!(
         "{}{}{}",
-        "enum LocalizationKey: String {\n", swift_file, "}"
+        "enum LocalizationKey: String {\n", localizable_case, "}"
     )
 }
 
