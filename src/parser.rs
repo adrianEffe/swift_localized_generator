5695fb2a9a0b2e3722b478830d12422a422e5676
adrianEffe <adrian.iosdev@gmail.com>
2023-05-08 17:03:29 +0300

    Update regex to support underscore

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub fn parse(content: &str) -> Vec<&str> {
-    let regex = Regex::new(r#""([a-zA-Z]+)"\s*=\s*"[^"]*";"#).unwrap();
+    let regex = Regex::new(r#""([a-zA-Z_]+)"\s*=\s*"[^"]*";"#).unwrap();
     let mut localizable_keys = Vec::new();
     for captures in regex.captures_iter(content) {
         let key = captures.get(1).unwrap().as_str();
@@ -16,7 +16,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_parse() {
-        let content = "\"homeTitle\" = \"Home\"; \"nextTitle\" = \"This is next\";";
-        assert_eq!(parse(content), vec!["homeTitle", "nextTitle"]);
+        let content = "\"homeTitle\" = \"Home\"; \"next_Title\" = \"This is next\";";
+        assert_eq!(parse(content), vec!["homeTitle", "next_Title"]);
     }
 }
